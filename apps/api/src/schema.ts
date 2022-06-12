92c0916cd5ef2a4f03818e45646684cb6a3dcee1
kamosah <68175563+kamosah@users.noreply.github.com>
2022-06-12 16:58:15 -0400

    Adding prisma client

--- apps/api/src/schema.ts
+++ apps/api/src/schema.ts
@@ -1,5 +1,7 @@
 import { gql } from 'apollo-server';
 
+import { prisma } from './db';
+
 export const typeDefs = gql`
   type Post {
     content: String
@@ -19,50 +21,39 @@ export const typeDefs = gql`
   }
 `;
 
-const posts = [
-  {
-    id: 1,
-    title: 'Subscribe to GraphQL Weekly for community news ',
-    content: 'https://graphqlweekly.com/',
-    published: true,
-  },
-  {
-    id: 2,
-    title: 'Follow DigitalOcean on Twitter',
-    content: 'https://twitter.com/digitalocean',
-    published: true,
-  },
-  {
-    id: 3,
-    title: 'What is GraphQL?',
-    content: 'GraphQL is a query language for APIs',
-    published: false,
-  },
-];
-
 export const resolvers = {
   Query: {
     feed: (parent, args) => {
-      return posts.filter((post) => post.published);
+      return prisma.post.findMany({
+        where: { published: true },
+      });
     },
     post: (parent, args) => {
-      return posts.find((post) => post.id === Number(args.id));
+      return prisma.post.findUnique({
+        where: {
+          id: Number(args.id),
+        },
+      });
     },
   },
   Mutation: {
     createDraft: (parent, args) => {
-      posts.push({
-        id: posts.length + 1,
-        title: args.title,
-        content: args.content,
-        published: false,
+      return prisma.post.create({
+        data: {
+          title: args.title,
+          content: args.content,
+        },
       });
-      return posts[posts.length - 1];
     },
     publish: (parent, args) => {
-      const postToPublish = posts.find((post) => post.id === Number(args.id));
-      postToPublish.published = true;
-      return postToPublish;
+      return prisma.post.update({
+        where: {
+          id: Number(args.id)
+        },
+        data: {
+          published: true
+        }
+      })
     },
   },
   Post: {
